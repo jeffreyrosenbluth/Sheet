a1ba0cee5f0753972c3d375f1c55dc4b0168b177
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2017-03-05 16:11:35 -0500

    comments

--- src/Sheet.hs
+++ src/Sheet.hs
@@ -1,12 +1,22 @@
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE FlexibleInstances  #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  Sheet
+-- Copyright   :  (c) 2017 Jeffrey Rosenbluth
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  jeffrey.rosenbluth@gmail.com
+--
+-- Tools for storing and settling shared payments.
+-------------------------------------------------------------------------------
+
 module Sheet where
 
-import           Data.Char
+import           Data.Char                    (isDigit)
 import           Data.List                    (sortOn)
 import           Data.Time.Calendar
 import           Data.Time.Format
@@ -18,6 +28,8 @@ import           Data.Serialize
 import           GHC.Generics
 import           Text.ParserCombinators.ReadP
 import           Text.Printf
+
+-- Types -----------------------------------------------------------------------
 
 type Name = String
 
@@ -45,22 +57,26 @@ data Payment = Payment
   , pmt  :: Rational
   } deriving (Show, Eq)
 
-printRational :: Rational -> String
-printRational = printf "% 10.2f" . dbl
+-- To String -------------------------------------------------------------------
+
+-- | Convert a rational nuber to a string with 2 decimal places.
+displayRational :: Rational -> String
+displayRational = printf "% 10.2f" . dbl
   where
     dbl :: Rational -> Double
     dbl = fromRational
- 
-displayPayment :: Payment -> String
-displayPayment (Payment f t p) = printRational p ++ ": " ++ f ++ " -> " ++ t ++ "\n"
 
+-- | 'Payment' to String.
+displayPayment :: Payment -> String
+displayPayment (Payment f t p) =
+  displayRational p ++ ": " ++ f ++ " -> " ++ t ++ "\n"
+
+-- | 'Entry' to String.
 displayEntry :: Entry -> String
 displayEntry = M.foldlWithKey'
-  (\b k v -> b ++ k ++ ": " ++ printRational v ++ "\n") ""
-  where
-    dbl :: Rational -> Double
-    dbl = fromRational
+  (\b k v -> b ++ k ++ ": " ++ displayRational v ++ "\n") ""
 
+-- | 'Event' to String
 displayEvent :: Event -> String
 displayEvent e
   =  printf "% 3d " (ident e)
@@ -70,21 +86,26 @@ displayEvent e
   ++ printf "% 10.2f " (fromRational (amount e) :: Double)
   ++ unwords (N.toList $ participants e) ++ "\n"
 
+-- | 'Sheet' to String, sorted by date.
 displaySheet :: Sheet -> String
 displaySheet = concatMap displayEvent . sortOn date
 
-mkLineItem :: Event -> Entry
-mkLineItem e = M.unionWith (+) paid owesMap
-  where
-    paid = M.singleton (payer e) (amount e)
-    owesMap = M.fromList owes
-    owes = map (\p -> (p, (-1) * amount e / fromIntegral n))
-               (N.toList $ participants e)
-    n    = length (participants e)
+-- Sheet Logic -----------------------------------------------------------------
 
+-- | Convert a line item into an entry in the 'Sheet' (ledger).
+mkLineItem :: Event -> Entry
+mkLineItem e = M.unionWith (+) paid (M.fromList owes)
+  where
+    paid    = M.singleton (payer e) (amount e)
+    owes    = map (\p -> (p, (-1) * amount e / fromIntegral n))
+                  (N.toList $ participants e)
+    n       = length (participants e)
+
+-- | Tally up all of the debts in the sheet.
 total :: Sheet -> Entry
 total sheet = foldr (M.unionWith (+)) M.empty (mkLineItem <$> sheet)
 
+-- | Find the smallest value in the map if it exists.
 minValue :: (Ord v, Ord k) => Map k v -> Maybe (k, v)
 minValue m
   | null m    = Nothing
@@ -93,6 +114,7 @@ minValue m
     f k v (k0, v0) = if v < v0  then (k, v) else (k0, v0)
     (k1, v1) = M.elemAt 0 m
 
+-- | Find the largets value in the map if it exists.
 maxValue :: (Ord v, Ord k) => Map k v -> Maybe (k, v)
 maxValue m
   | null m    = Nothing
@@ -101,6 +123,7 @@ maxValue m
     f k v (k0, v0) = if v > v0  then (k, v) else (k0, v0)
     (k1, v1) = M.elemAt 0 m
 
+-- | Pair the largest debtor with the largest lender and ajust the 'Sheet' (ledger).
 pairOff :: Entry -> Maybe (Payment, Entry)
 pairOff e = do
   (f, a) <- maxValue e
@@ -110,6 +133,7 @@ pairOff e = do
       then (Payment t f (negate b), M.delete t  . M.adjust (+ b) f $ e)
       else (Payment t f a, M.adjust (+ a) t . M.delete f $ e)
 
+-- | Create a list of transfer payments so that there are no debts left in the 'Sheet'.
 reconcile :: Entry -> [Payment]
 reconcile e = if M.size e >= 2
   then
@@ -118,11 +142,13 @@ reconcile e = if M.size e >= 2
       Just (p, e') -> p : reconcile e'
   else []
 
+-- | Remove the 'Event' with the given indentifier from the 'Sheet'
 deleteEntry :: Sheet -> Int -> Sheet
 deleteEntry s n = filter ((/= n) . ident) s
 
 -- Parser ----------------------------------------------------------------------
 
+-- | Convert an event string to an 'Event'.
 parseEvent :: ReadP Event
 parseEvent = do
   dt   <- parseDate <* sep
@@ -132,6 +158,7 @@ parseEvent = do
   ps   <- parseParticipants
   return $ Event 0 desc dt pyr ps amt
 
+-- | parse a Date.
 parseDate :: ReadP Day
 parseDate = do
   month <- posInt <* slash
@@ -139,36 +166,39 @@ parseDate = do
   year  <- parseYear
   return $ fromGregorian year month day
 
+-- | Parse a rational number.
 parseAmount :: ReadP Rational
 parseAmount = do
   s <- notComma
   return $ (toRational (100 * read s :: Double)) / 100
 
+-- | Parse a non-empty list of 'Name's.
 parseParticipants :: ReadP (NonEmpty Name)
 parseParticipants = do
   ns <- notComma
   return $ N.fromList (words ns)
 
+-- | Parse a comma.
 comma :: ReadP Char
 comma = char ','
 
+-- | Parse a forward slash.
 slash :: ReadP Char
 slash = char '/'
 
+-- | Parse until reaching a comma.
 notComma :: ReadP String
 notComma = munch1 (/= ',')
 
+-- | Separator parser.
 sep :: ReadP Char
 sep = comma <* skipSpaces
 
+-- | Parse a positive Int.
 posInt :: ReadP Int
-posInt = readS_to_P f
-  where
-    f xs
-      | null ns   = []
-      | otherwise = [(read ns, rest)]
-      where (ns, rest) = span isDigit xs
+posInt = read <$> many1 (satisfy isDigit)
 
+-- | Parse a year to an Integer.
 parseYear :: ReadP Integer
 parseYear = readS_to_P f
   where
