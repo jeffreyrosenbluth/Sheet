1e42275e1800b971f4cf884920a9b28951bdb931
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2017-03-10 13:59:12 -0500

    more printing

--- src/Report.hs
+++ src/Report.hs
@@ -18,8 +18,11 @@ import           Sheet
 
 import           Data.Monoid
 import           Lucid
-import qualified Data.Map.Strict as M
-import           Data.Text.Lazy (Text)
+import qualified Data.Map.Strict    as M
+import           Data.List          (sortOn)
+import qualified Data.List.NonEmpty as N
+import           Data.Text.Lazy     (Text)
+import           Data.Time.Format   (defaultTimeLocale, formatTime)
 
  
 header :: Html ()
@@ -35,7 +38,10 @@ report :: Sheet -> Html ()
 report sheet =
   html_ $
        head_ header
-    <> body_ (bodyHeader <> reportEntry entry)
+    <> body_ (  bodyHeader
+             <> reportEntry entry
+             <> reportSheet sheet
+             )
   where
     entry = total sheet
 
@@ -45,6 +51,25 @@ reportEntry entry = ul_ $ M.foldMapWithKey item entry
     item :: String -> Rational -> Html ()
     item s r = li_ $ toHtml (s <> (displayRational r))
 
+reportSheet :: Sheet -> Html ()
+reportSheet s =
+  div_ $ table_ (mconcat $ map reportEvent (sortOn date s))
+
+reportEvent :: Event -> Html ()
+reportEvent e =
+  tr_ (  td_ dt
+      <> td_ (toHtml $ description e)
+      <> td_ (toHtml $ payer e)
+      <> td_ amt
+      <> td_ ps
+      )
+  where
+    dt = toHtml $ formatTime defaultTimeLocale "%D " (date e)
+    amt = toHtml $ displayRational (amount e)
+    ps  = toHtml $ unwords (N.toList . N.sort $ participants e) 
+   
+
+
 renderReport :: Sheet -> Text
 renderReport = renderText . report
- 
+
