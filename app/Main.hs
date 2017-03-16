f2642ad7e0fe634d93632495d2e5b9377e5ebf85
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2017-03-16 08:36:09 -0400

    add comments

--- app/Main.hs
+++ app/Main.hs
@@ -23,7 +23,7 @@ import           Control.Monad                (unless)
 import           Control.Monad.State.Strict
 import qualified Data.ByteString              as BS
 import           Data.Serialize               (encode, decode)
-import qualified Data.Text.Lazy.IO            as T 
+import qualified Data.Text.Lazy.IO            as T
 import           System.Console.Haskeline
 import           System.Directory             (doesFileExist)
 import           System.Environment           (getArgs)
@@ -32,9 +32,9 @@ import           Text.ParserCombinators.ReadP (readP_to_S)
 -- Types -----------------------------------------------------------------------
 
 data Model = Model
-  { nextId :: Int
+  { prevId :: Int
   , sheet  :: Sheet
-  , file   :: String
+  , file   :: FilePath
   }
 
 --------------------------------------------------------------------------------
@@ -50,18 +50,23 @@ main = do
     (fname:_) -> do
       (m, err) <- openSheet fname
       putStrLn err
-      runStateT (runInputT sheetSettings repl) m 
+      runStateT (runInputT sheetSettings repl) m
    where
      sheetSettings = defaultSettings {historyFile = Just "sheetit_history.txt"}
 
-withExt :: String -> String -> String
+-- | Add an extension to a 'FilePath' if it does not alread have one.
+withExt :: FilePath -> FilePath -> FilePath
 withExt ext base
   | '.' `elem` base = base
-  | otherwise  = base ++ "." ++ ext
+  | otherwise       = base ++ "." ++ ext
 
-setModel :: [Event] -> String -> Model
+-- | Given a sheet, return a function which takes a 'FilePath' and returns a Model.
+--   The 'prevId' field is set to the maximum of the ones already in the map.
+setModel :: Sheet -> String -> Model
 setModel events = Model (maximum $ map ident events) events
 
+-- | Decode a sheet and use it and the file name to set the model.
+--   Return the 'Model' along with a message indicating success or failure.
 openSheet :: FilePath -> IO (Model, String)
 openSheet fname = do
   let fn = withExt "sht" fname
@@ -70,16 +75,17 @@ openSheet fname = do
     then do
       efile <- BS.readFile fn
       return $ case decode efile of
-        Left err     -> (Model 0 [] "", "*** CANNOT DECODE FILE: " ++ err ++ " ***")
+        Left msg     -> (Model 0 [] "", "*** CANNOT DECODE FILE: " ++ msg ++ " ***")
         Right events -> (setModel events fn, "*** " ++ fn ++ " LOADED SUCCESSFULLY ***")
     else
       return (Model 0 [] "", "*** FILE: " ++ fn ++ " DOES NOT EXIST. ***")
 
 addEvent :: Event -> Model -> Model
-addEvent e m = m {sheet = (e {ident = n}) : sheet m, nextId = n}
+addEvent e m = m {sheet = (e {ident = n}) : sheet m, prevId = n}
   where
-    n = nextId m + 1
+    n = prevId m + 1
 
+-- | We make InputT m an instance of 'MonadState' so we don't have to constantly lift get, put etc.
 instance MonadState s m => MonadState s (InputT m) where
   get   = lift get
   put   = lift . put
@@ -104,20 +110,20 @@ repl = do
         (c, _)    : _ -> process c
 
 process :: Command -> Repl ()
-process Show           = get >>= outputStrLn . displaySheet . sheet >> repl
-process Total          = get >>= outputStrLn . displayEntry . total . sheet >> repl
-process Reconcile      = get >>= outputStrLn . concatMap displayPayment
+process  Show          = get >>= outputStrLn . displaySheet . sheet >> repl
+process  Total         = get >>= outputStrLn . displayEntry . total . sheet >> repl
+process  Reconcile     = get >>= outputStrLn . concatMap displayPayment
                                              . reconcile . total . sheet >> repl
-process Help           = outputStrLn help >> repl
+process  Help          = outputStrLn help >> repl
 process (Report fname) = get >>= liftIO . T.writeFile (withExt "html" fname)
-                                       . renderReport . sheet >> repl
+                                        . renderReport . sheet >> repl
 process (New fname)    = modify (\m -> m {file = (withExt "sht" fname)}) >> repl
 process (Add event)    = modify (addEvent event) >> save >> repl
 process (Delete n)     = modify (\m -> m {sheet = deleteEntry (sheet m) n}) >> save >> repl
-process Quit           = outputStrLn "Goodbye"
+process  Quit          = outputStrLn "Goodbye"
 process (Open fname)   = do
-  (m, err) <- liftIO $ openSheet fname
-  outputStrLn err
+  (m, msg) <- liftIO $ openSheet fname
+  outputStrLn msg
   unless (null . sheet $ m) (put m)
   repl
 
